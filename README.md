03bf2a497319adee1421c2d02157ef54be7160aa
SinriMac <e.joshua.s.e@gmail.com>
2016-03-30 08:00:40 +0800

    追加 根据源数据计算
    虚无。

--- README.md
+++ README.md
@@ -36,23 +36,31 @@ This function to generate a new table instance in the preset div. Parameter `dat
 
 This function is to make the data in table body to an JSON array and stringify it to return.
 
+`function refresh()`
+
+Force to recompute value of cells.
+
 ### Data Format
 
 Data Matrix is a json object which contains two keys, `title` and `body`. Both of the two contain an array with one or more item, one item for one row. Item for title lines could be String or object with three possible keys of `name`, `rowspan` and `colspan`, but `name` must be there. Item for body lines should be a value to display. For example:
 
 	var dm={
 		title:[
-			[{name:'No.',rowspan:2},{name:'Part I',colspan:2},{name:'Part II',colspan:2}],
-			['AName','BName','CName','DName']
+			[{name:'No.',rowspan:2},{name:'Part I',colspan:2},{name:'Part II',colspan:2},{name:"FORMULA",colspan:2}],
+			['AName','BName','CName','DName','FORMULA','EXPECTED']
 		],
 		body:[
-			['0','A','B','C','D'],
-			['1','A1','B1','C1','D1'],
-			['2','A2','B2','C2','D2'],
-			['3','A3','B3','C3','D3'],
-			['4','A4','B4','C4','D4'],
-			['5','A5','B5','C5','D5'],
+			['0','1','2','3','4','5','=5'],
+			['10','11','12','13','14','=[1,1]','11'],
+			['20','21','22','23','24','=[2,3]+[2,4]/[2,2]',''+(23+24/22)],
+			['30','31','32','33','34','=count(0,0,5,1)','12'],
+			['40','41','42','34','44','=sum(0,0,5,1)','306'],
+			['50','51','52','53','54','=ave(0,0,5,1)','25.5'],
 		]
 	};
 
+In source data matrix json, you would find those string values begin with equal char `=`, which are defined as formulas. Formula functions now include `count`, `sum`, `ave`, `min`, `max`. Each of the functions has four parameters, `r1`, `c1`, `r2`, `c2`. For example, parameter `(0,0,1,1)` includes the sub matrix of 
+
+	[0,0]	[0,1]
+	[1,0]	[1,1]
 
--- SinriTable.js
+++ SinriTable.js
@@ -55,11 +55,19 @@ var SinriTable=function(table_div_id,editable){
 					tr.setAttribute('row_index',r_index);
 					for(var c_index=0;c_index<row.length;c_index++){
 						var col=row[c_index];
+						var formula=col;
+						// if(col.length>0 && col[0]=='='){
+							// formula=col;
+						// }else{
+							// formula="";
+						// }
 
 						var td = document.createElement("td");
 						td.setAttribute('row_index',r_index);
 						td.setAttribute('col_index',c_index);
 						td.setAttribute('coor_hash',r_index+'_'+c_index);
+
+						td.setAttribute('formula',formula);
 
 						if(!that.editable){
 							var textNode = document.createTextNode(col);
@@ -70,11 +78,13 @@ var SinriTable=function(table_div_id,editable){
 							textbox.setAttribute('col_index',c_index);
 							textbox.setAttribute('coor_hash',r_index+'_'+c_index);
 							textbox.setAttribute('class','SinriTableCellInputInactive');
+
 							textbox.setAttribute('value',col);
 
 							if(typeof onCellEdited=='function'){
 								textbox.addEventListener('input',function(){
 									onCellEdited(r_index,c_index,this.value);
+									that.refresh();
 								});
 							}
 
@@ -91,6 +101,158 @@ var SinriTable=function(table_div_id,editable){
 		}
 
 		table_div.appendChild(table);
+
+		this.refresh();
+	}
+
+	this.refresh=function(){
+		var table_div=document.getElementById(that.table_div_id);
+		var table=table_div.getElementsByTagName('table');
+		table=table[0];
+		var thead=table.getElementsByTagName('thead');
+		thead=thead[0];
+		var tbody=table.getElementsByTagName('tbody');
+		tbody=tbody[0];
+		console.log("REFRESH BODY: ");
+		console.log(tbody);
+		var body_tr_list=tbody.getElementsByTagName('tr');
+		for(var tr_index=0;tr_index<body_tr_list.length;tr_index++){
+			var the_tr=body_tr_list[tr_index];
+			var the_td_list=the_tr.getElementsByTagName('td');	
+			for(var td_index=0;td_index<the_td_list.length;td_index++){
+				var the_td=the_td_list[td_index];
+				// var tb=the_td.getElementsByTagName('input');
+				// var cell_str='';
+				// if(tb && tb.length>0){
+				// 	tb=tb[0];
+				// 	cell_str = (tb.value);
+				// }else{
+				// 	cell_str = (the_td.textContent);
+				// }
+				console.log("REFRESH["+tr_index+","+td_index+"]->");
+				var value=that.getCellValue(tr_index,td_index);
+				console.log(value);
+				var formula=that.getCellFormula(tr_index,td_index);
+				console.log(formula);
+				if(formula.length>0 && formula[0]=='='){
+					value=that.parseCellString(formula.slice(1));
+				}
+				that.setCellString(tr_index,td_index,value);
+			}
+		}
+	}
+
+	this.getCellTd=function(row,col){
+		console.log("getCellString("+row+","+col+")")
+		var table_div=document.getElementById(that.table_div_id);
+		var table=table_div.getElementsByTagName('table');
+		table=table[0];
+		var thead=table.getElementsByTagName('thead');
+		thead=thead[0];
+		var tbody=table.getElementsByTagName('tbody');
+		tbody=tbody[0];
+
+		var body_tr_list=tbody.getElementsByTagName('tr');
+		var the_tr=body_tr_list[row];
+		var the_td_list=the_tr.getElementsByTagName('td');
+		var td=the_td_list[col];
+
+		return td;
+	}
+
+	this.getCellFormula=function(row,col){
+		var td=that.getCellTd(row,col);
+
+		var formula=td.getAttribute('formula');
+		return formula;
+	}
+
+	this.getCellString=function(row,col){
+		var td=that.getCellTd(row,col);
+
+		var tb=td.getElementsByTagName('input');
+		if(tb && tb.length>0){
+			tb=tb[0];
+			return (tb.value);
+		}else{
+			return (td.textContent);
+		}
+	}
+
+	this.setCellString=function(row,col,v){
+		var td=that.getCellTd(row,col);
+
+		var tb=td.getElementsByTagName('input');
+		if(tb && tb.length>0){
+			tb=tb[0];
+			tb.value=v;
+		}else{
+			td.textContent=v;
+		}
+	}
+
+	this.parseCellCodeToValue=function(cell_code){
+		console.log("parseCellCodeToValue("+cell_code+")")
+		var cc=cell_code.match(/\[(\d+),(\d+)\]/);
+		console.log("parseCellCodeToValue("+cell_code+") cc = ");
+		console.log(cc);
+		return that.getCellValue(cc[1],cc[2]);
+	}
+
+	this.getCellValue=function(row,col){
+		console.log("getCellValue("+row+","+col+")")
+		var str=that.getCellString(row,col);
+		if(str[0]!='='){
+			return str;
+		}
+		else {
+			return that.parseCellString(str.slice(1));
+		}
+	}
+
+	this.parseCellString=function(str){
+		//PART I FORMULA
+		var re=/(count|sum|ave|min|max)\(\d+,\d+,\d+,\d+\)/.exec(str);
+		console.log("parseCellString("+str+") re1=");
+		console.log(re);
+		if(re){
+			var first=re.index;
+			var length=re[0].length;
+			var k=eval('that.formula().'+re[0]);
+			var new_str=str.slice(0,first)+''+k+''+str.slice(first+length);
+			return that.parseCellString(new_str);
+		}
+
+		//PART II REPLACE CELL
+		var re=/\[\d+,\d+\]/.exec(str);
+		console.log("parseCellString("+str+") re2=");
+		console.log(re);
+		if(re){
+			var first=re.index;
+			var length=re[0].length;
+			var k=that.parseCellCodeToValue(re[0]);
+			var new_str=str.slice(0,first)+''+k+''+str.slice(first+length);
+			return that.parseCellString(new_str);
+		}else{
+			var v1 = that.safeEval(str);
+			var v2 = that.safeEval(v1);
+			while(v1!=v2){
+				v1=v2;
+				v2=that.safeEval(v1);
+			}
+			return v2;
+		}
+	}
+
+	this.safeEval=function(code){
+		try {
+			return eval(code);
+		}catch (e) {
+			if(e instanceof SyntaxError){
+				return code;
+			}
+			return code;
+		}
 	}
 
 	this.bodyJson=function(){
@@ -126,5 +288,79 @@ var SinriTable=function(table_div_id,editable){
 		}
 		return JSON.stringify(json_array);
 	}
+
+	//////
+
+	//INNER FORMULA FUNCTIONS
+	this.formula = function(){
+		var there=that;
+		this.turnScaleToArray=function(r1,c1,r2,c2){
+			console.log("turnScaleToArray("+r1+","+c1+","+r2+","+c2+")->("+Math.min(r1,r2)+","+Math.min(c1,c2)+","+Math.max(r1,r2)+","+Math.max(c1,c2)+")");
+			var list=[];
+			for(var row=Math.min(r1,r2);row<=Math.max(r1,r2);row++){
+				for(var col=Math.min(c1,c2);col<=Math.max(c1,c2);col++){
+					list.push(there.getCellValue(row,col));
+				}
+			}
+			console.log(list);
+			return list;
+		}
+		this.count=function(r1,c1,r2,c2){
+			var list=this.turnScaleToArray(r1,c1,r2,c2);
+			return list.filter(function(v){return v!=''}).length;
+		}
+		this.sum=function(r1,c1,r2,c2){
+			var list=this.turnScaleToArray(r1,c1,r2,c2);
+			var sum=0;
+			list.forEach(function(v){
+				var vv=parseFloat(v);
+				if(!isNaN(vv))sum=sum+vv
+			});
+			return sum;
+		}
+		this.ave=function(r1,c1,r2,c2){
+			var list=this.turnScaleToArray(r1,c1,r2,c2);
+			var sum=0;
+			var count=0;
+			list.forEach(function(v){
+				var vv=parseFloat(v);
+				if(!isNaN(vv)){
+					sum=sum+vv;
+					count=count+1;
+				}
+			});
+			return sum/count;
+		}
+		this.min=function(r1,c1,r2,c2){
+			var list=this.turnScaleToArray(r1,c1,r2,c2);
+			var theMin=null;
+			list.forEach(function(v){
+				var vv=parseFloat(v);
+				if(!isNaN(vv)){
+					if(theMin==null || theMin>vv){
+						theMin=vv;
+					}
+				}
+			});
+			return theMin;
+		}
+		this.max=function(r1,c1,r2,c2){
+			var list=this.turnScaleToArray(r1,c1,r2,c2);
+			var theMax=null;
+			list.forEach(function(v){
+				var vv=parseFloat(v);
+				if(!isNaN(vv)){
+					if(theMax==null || theMax<vv){
+						theMax=vv;
+					}
+				}
+			});
+			return theMax;
+		}
+		return this;
+	}
+
+	//////
+
 	return this;
 }
